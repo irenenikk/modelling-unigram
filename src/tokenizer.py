4e90ee59be5643b366e358313c62427606448cea
Tiago Pimentel <tiagopms@gmail.com>
2020-01-20 15:16:31 +0000

    Fixes for linter

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -8,24 +8,29 @@ import gensim.utils
 
 
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
-parser = argparse.ArgumentParser()
-# Wikipedia
-parser.add_argument(
-    "--wikipedia-raw-file", type=str,
-    help="The file containing the wiki json files from which to read")
-parser.add_argument(
-    "--wikipedia-tokenized-file", type=str,
-    help="The file in which wikipedia tokenized results should be")
-parser.add_argument(
-    "--dump-size", type=int, default=1000, required=False,
-    help="The number of articles to be processed between each dump")
-parser.add_argument(
-    "--max-articles", type=int, default=None, required=False,
-    help="The maximum number of articles to be processed")
-# Model
-parser.add_argument(
-    "--spacy", default='xx_ent_wiki_sm', required=False,
-    help="The name of the spaCy language model (default is multilingual)")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    # Wikipedia
+    parser.add_argument(
+        "--wikipedia-raw-file", type=str,
+        help="The file containing the wiki json files from which to read")
+    parser.add_argument(
+        "--wikipedia-tokenized-file", type=str,
+        help="The file in which wikipedia tokenized results should be")
+    parser.add_argument(
+        "--dump-size", type=int, default=1000, required=False,
+        help="The number of articles to be processed between each dump")
+    parser.add_argument(
+        "--max-articles", type=int, default=None, required=False,
+        help="The maximum number of articles to be processed")
+    # Model
+    parser.add_argument(
+        "--spacy", default='xx_ent_wiki_sm', required=False,
+        help="The name of the spaCy language model (default is multilingual)")
+
+    return parser.parse_args()
 
 
 def load_spacy(spacy_option):
@@ -61,10 +66,9 @@ def process_sentences(sentences, spacy_tokenizer):
         if len(target_tokens) > 1:
             parsed_sentences += [' '.join(target_tokens)]
 
-    if len(parsed_sentences) > 10:
-        return ' '.join(parsed_sentences)
-    else:
+    if len(parsed_sentences) <= 10:
         return None
+    return ' '.join(parsed_sentences)
 
 
 def process_article(article, spacy_tokenizer):
@@ -100,7 +104,8 @@ def tokenize_wikipedia(src_fname, tgt_fname, spacy_tokenizer,
 
     logging.info('Preprocessing wikipedia')
     with gensim.utils.open(src_fname, 'rb') as f:
-        for article_id, article in tqdm(enumerate(f), total=n_articles, desc='Tokenizing wikipedia', mininterval=.2):
+        for article_id, article in tqdm(enumerate(f), total=n_articles,
+                                        desc='Tokenizing wikipedia', mininterval=.2):
             processed_article = process_article(article, spacy_tokenizer)
             if processed_article is not None:
                 processed_articles += [processed_article]
@@ -124,10 +129,11 @@ def process(src_fname, tgt_fname, spacy_option, dump_size, max_articles):
 
 
 def main():
-    args = parser.parse_args()
+    args = get_args()
     logging.info(args)
 
-    process(args.wikipedia_raw_file, args.wikipedia_tokenized_file, args.spacy, args.dump_size, args.max_articles)
+    process(args.wikipedia_raw_file, args.wikipedia_tokenized_file,
+            args.spacy, args.dump_size, args.max_articles)
 
 
 if __name__ == '__main__':
