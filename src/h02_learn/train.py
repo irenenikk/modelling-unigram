2eae59f7d59ba63f0fa8f61032f329318ccb1f30
Tiago Pimentel <tiagopms@gmail.com>
2020-01-21 21:20:39 +0000

    Moving train info class to its own file

--- src/h02_learn/train.py
+++ src/h02_learn/train.py
@@ -6,9 +6,11 @@ import torch.optim as optim
 sys.path.append('./src/')
 from h02_learn.dataset import get_data_loaders
 from h02_learn.model import LstmLM
+from h02_learn.train_info import TrainInfo
 from util import argparser
 from util import util
 from util import constants
+
 
 
 def get_args():
@@ -77,62 +79,11 @@ def evaluate(evalloader, model, alphabet):
     return result
 
 
-class TrainInfo:
-    batch_id = 0
-    running_loss = []
-    best_loss = float('inf')
-    best_batch = 0
-
-    def __init__(self, wait_iterations, eval_batches):
-        self.wait_iterations = wait_iterations
-        self.eval_batches = eval_batches
-
-    @property
-    def finish(self):
-        return (self.batch_id - self.best_batch) >= self.wait_iterations
-
-    @property
-    def eval(self):
-        return (self.batch_id % self.eval_batches) == 0
-
-    @property
-    def max_epochs(self):
-        return self.best_batch + self.wait_iterations
-
-    @property
-    def avg_loss(self):
-        return sum(self.running_loss) / len(self.running_loss)
-
-    def new_batch(self, loss):
-        self.batch_id += 1
-        self.running_loss += [loss]
-
-    def is_best(self, dev_loss):
-        if dev_loss < self.best_loss:
-            self.best_loss = dev_loss
-            self.best_batch = self.batch_id
-            return True
-
-        return False
-
-    def reset_loss(self):
-        self.running_loss = []
-
-    def print_progress(self, dev_loss):
-        # avg_loss = sum(running_loss) / len(running_loss)
-        # max_epochs = best_batch + wait_iterations
-        print('(%05d/%05d) Training loss: %.4f Dev loss: %.4f' %
-              (self.batch_id / 100, self.max_epochs / 100, self.avg_loss, dev_loss))
-        self.reset_loss()
-
-
 def train(trainloader, devloader, model, alphabet, eval_batches, wait_iterations):
     # optimizer = optim.AdamW(model.parameters())
     optimizer = optim.Adam(model.parameters())
     criterion = nn.CrossEntropyLoss(ignore_index=alphabet.char2idx('PAD')) \
         .to(device=constants.device)
-    # batch_id, running_loss = 0, []
-    # best_loss, best_batch = float('inf'), 0
     train_info = TrainInfo(wait_iterations, eval_batches)
 
     while not train_info.finish:
