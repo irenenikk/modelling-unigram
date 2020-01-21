b9e5c9bd162fbc45b76a4f0b1f50f2f98c38fa29
Tiago Pimentel <tiagopms@gmail.com>
2020-01-21 21:22:00 +0000

    Removed unused comments

--- src/h02_learn/dataset/tokens.py
+++ src/h02_learn/dataset/tokens.py
@@ -5,10 +5,6 @@ from .base import BaseDataset
 
 
 class TokenDataset(BaseDataset):
-    # def __init__(self, data, folds):
-    #     self.data = data
-    #     self.folds = folds
-    #     self.process_data(data)
 
     def process_train(self, data):
         folds_data, alphabet, _ = data
@@ -33,14 +29,3 @@ class TokenDataset(BaseDataset):
             begin = end
 
         return word_idxs, end
-
-    # def get_word_idx(self, word):
-    #     return [self.alphabet.char2idx('SOS')] + \
-    #         self.alphabet.word2idx(word) + \
-    #         [self.alphabet.char2idx('EOS')]
-
-    # def __len__(self):
-    #     return self.n_instances
-
-    # def __getitem__(self, index):
-    #     return self.word_idxs[index]
--- src/h02_learn/dataset/types.py
+++ src/h02_learn/dataset/types.py
@@ -1,15 +1,9 @@
 import torch
-# from torch.utils.data import Dataset
 
 from .base import BaseDataset
 
 
 class TypeDataset(BaseDataset):
-    # def __init__(self, data, folds):
-    #     self.data = data
-    #     self.folds = folds
-    #     self.process_data(data)
-    #     self.n_instances = len(self.words)
 
     def process_train(self, data):
         folds_data, alphabet, _ = data
@@ -23,14 +17,3 @@ class TypeDataset(BaseDataset):
         self.word_eval = self.word_train
         self.weights = [1] * len(self.word_eval)
         self.eval_instances = len(self.word_eval)
-
-    # def get_word_idx(self, word):
-    #     return [self.alphabet.char2idx('SOS')] + \
-    #         self.alphabet.word2idx(word) + \
-    #         [self.alphabet.char2idx('EOS')]
-
-    # def __len__(self):
-    #     return self.n_instances
-
-    # def __getitem__(self, index):
-    #     return self.word_idxs[index]
